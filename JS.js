3128cd03bbec626caf3844df23443965e7c26355
Anjana Sejwal <anjana@CISIVE-INT.LOCAL>
2020-11-21 22:54:49 +0530

    Aajayegi chalani

---
+++ JS.js
@@ -0,0 +1,28 @@
+/*
+    LOGIN SECTION
+*/
+
+function login(f) {
+    var RE_Name = /^[a-zA-Z]$/;
+    var i;
+    for (i = 0; i < f.elements.length; i++) {
+        if (f.elements[i].value == "") {
+            alert("Field should not be empty!");
+            break;
+        }
+    }
+    if (i != f.elements.length) {
+        f.elements[i].focus();
+        return;
+    }
+    /*
+    for (i = 0; i < f.elements.length; i++) {
+        if (!RE[i].test(f.elements[i])) {
+            document.getElementById("")
+        }
+    }*/
+    if (!RE_Name.test(f.elements[0].value) && !RE_Name.test(f.elements[0].value)) {
+        document.getElementById("error_name").innerHTML = 'Name should contain only alphabets!';
+        f.elements[0].focus();
+    }
+}
